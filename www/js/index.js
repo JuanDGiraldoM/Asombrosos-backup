4fc864881c9200c2b6cb579d287a0bdd3027837c
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2018-10-29 09:41:20 -0500

    Fondo de carga

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,39 @@
 window.onload = inicializar();
+var acerca,juego;
 
 function inicializar()
 {
     setTimeout('cargarJuego()', 5000);
+    initVariables();
+    initEventos();
 }
 
 function cargarJuego()
 {
     location.href = "#menu";
+}
+
+function initVariables()
+{
+    acerca = document.getElementById('about');
+    juego = document.getElementById('play');
+}
+
+function initEventos()
+{
+    acerca.addEventListener('click', toAbout);
+    juego.addEventListener('click', toGame);
+}
+
+function toAbout()
+{
+    location.href = "#viewAbout";
+}
+
+function toGame()
+{
+    location.href = "#initGame1";
+    setTimeout(function() {
+        location.href = "#initGame2";
+    }, 12000);
 }
