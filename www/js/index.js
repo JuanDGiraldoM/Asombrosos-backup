31f740fdd49ae341af2e8d71376af55df7031f85
Juan Jose Gomez <juanjoguaro96@gmail.com>
2018-10-29 19:05:12 -0500

    Se agregó la pantalla galeria y el boton para ingresar

--- www/js/index.js
+++ www/js/index.js
@@ -18,6 +18,8 @@ function initVariables()
     acerca = document.getElementById('about');
     juego = document.getElementById('play');
     back = document.getElementById('back');
+    galeria = document.getElementById('btnGalery');
+    back2MainScreen = document.getElementById('back2MainScreen');
 }
 
 function initEventos()
@@ -25,6 +27,8 @@ function initEventos()
     acerca.addEventListener('click', toAbout);
     juego.addEventListener('click', toGame);
     back.addEventListener('click', function() { location.href = "#menu"});
+    galeria.addEventListener('click',() => {location.href = "#galery"});
+    back2MainScreen.addEventListener('click', () => {location.href = "#menu"});
 }
 
 function toAbout()
@@ -40,4 +44,7 @@ function toGame()
         location.href = "#initGame2";
         setTimeout(function() { location.href = "#listGames";}, 12000);
     }, 12000);
+}
+function toGalery(){
+    location.href = "#galery";
 }
