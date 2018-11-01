f0ec8d7d83524572e36b11b7f9c4095f4610facc
ANDRES\Andres <andres.arl77@gmail.com>
2018-10-31 21:10:08 -0500

    Game function added

--- www/js/antonyms.js
+++ www/js/antonyms.js
@@ -3,14 +3,16 @@ var ma_arrayCard=[];
 ma_arrayCard[0]=["img/antonyms/anger.png","ma1"];
 ma_arrayCard[1]=["img/antonyms/calm.png","ma1"];
 ma_arrayCard[2]=["img/antonyms/fear.png","ma2"];
-ma_arrayCard[3]=["img/antonyms/fear.png","m4"];
-ma_arrayCard[4]=["img/antonyms/happiness.png","m3"];
-ma_arrayCard[5]=["img/antonyms/sadness.png","m3"];
-ma_arrayCard[6]=["img/antonyms/surprise.png","m2"];
-ma_arrayCard[7]=["img/antonyms/surprise.png","m4"];
+ma_arrayCard[3]=["img/antonyms/pride.png","ma4"];
+ma_arrayCard[4]=["img/antonyms/happiness.png","ma3"];
+ma_arrayCard[5]=["img/antonyms/sadness.png","ma3"];
+ma_arrayCard[6]=["img/antonyms/surprise.png","ma2"];
+ma_arrayCard[7]=["img/antonyms/disappointment.png","ma4"];
 
 var ma_card1, ma_card2, ma_card3, ma_card4, ma_card5, ma_card6, ma_card7, ma_card8;
 var ma_currentImg;
+var MA_COUNT;
+var MA_COUNTFINAL=4;
 
 window.onload=init;
 
@@ -56,6 +58,8 @@ function init(){
     ma_card7.addEventListener('click', verifyMatch);
     ma_card8.addEventListener('click', verifyMatch);
 
+    MA_COUNT=0;
+
 }
 
 function shuffle(a) {
@@ -72,10 +76,22 @@ function shuffle(a) {
 function verifyMatch(){
     if(!ma_currentImg){
         ma_currentImg=this;
-        var ma_id=String(this.id);
         new TweenMax.fromTo(ma_currentImg, 2, {width:"79%"},{width:"85%"});
-    }else{
-
+    }else if(ma_currentImg.id != this.id){
+        var ma_attr=ma_currentImg.getAttribute("data-ma");
+        var ma_sattr=this.getAttribute("data-ma");
+        if(!(ma_attr == ma_sattr)){
+            new TweenMax.fromTo(ma_currentImg, 2, {width:"85%"},{width:"70%"});
+        }else{
+            new TweenMax.fromTo(this, 2, {width:"79%"},{width:"85%"});
+            this.removeEventListener('click',verifyMatch);
+            ma_currentImg.removeEventListener('click',verifyMatch);
+            MA_COUNT++;
+            if(MA_COUNT == MA_COUNTFINAL){
+                console.log("Ganaste");
+            }
+        }
+        ma_currentImg=undefined;
     }
 
 }
