1ffb8b1933d56992323d70d858ef9135477adf52
marianamendivil <mariana.mendidu@gmail.com>
2018-11-02 10:28:57 -0500

    bichito

--- www/js/runner.js
+++ www/js/runner.js
@@ -3,14 +3,43 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var ctx = canvas.getContext('2d');
 	var cWidth = canvas.width;
 	var cHeight = canvas.height;
-	var playerX = 50;
-	var playerY = cHeight-30;
+	var playerX = cWidth*0.3;
+	var playerY = cHeight*0.2;
 	var yVelocity = 0.3;
-	var yLimit = cHeight-30;
+	var xVelocity = -0.2;
+	var yLimit = cHeight*1.5;
+
+	var frames = 0;
+
+	function onDrawFrame(ctx, frame) {
+	// Match width/height to remove distortion
+	ctx.canvas.width  = ctx.canvas.offsetWidth;
+	ctx.canvas.height = ctx.canvas.offsetHeight;
+
+	// Determine how many pikachus will fit on screen
+	//var n = Math.floor((ctx.canvas.width)/150)
+
+	//for(var x = 0; x < n; x++) {
+	// Draw a pikachu
+	var left = 150;
+	ctx.globalCompositeOperation = 'source-over';
+	ctx.drawImage(frame.buffer, playerX + left, playerY, 83, 144);
+
+	// Composite a color
+	//var hue = (frames * 10 + x * 50) % 360;
+	//ctx.globalCompositeOperation = 'source-atop';
+	//ctx.fillStyle = 'hsla(' + hue + ', 100%, 50%, 0.5)';
+	//ctx.fillRect(left, 0, 150, this.height);
+	//}
+	frames++;
+	}
+
+    gifler('img/RAYO-CORRIENDO-PERSONAJE.gif')
+    .frames('canvas.noPikachu', onDrawFrame);
 
 	function startup() {
 		var el = document.getElementsByTagName("canvas")[0];
-		el.addEventListener("touchstart", ()=>{yVelocity = -3; playerY-=10}, false);
+		el.addEventListener("touchstart", ()=>{yVelocity = -2; xVelocity = 0.5; playerY-=10}, false);
 		//el.addEventListener("touchend", handleEnd, false);
 		//el.addEventListener("touchcancel", handleCancel, false);
 		//el.addEventListener("touchleave", handleLeave, false);
@@ -18,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	}
 
 	gravity = function (velocity) {
-    	return velocity + 0.1;
+    	return velocity + 0.08;
 	};
 	
 	drawRect = function (x, y, radius, color) {
@@ -41,12 +70,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		} else if (playerY < yLimit) {
 			yVelocity = gravity(yVelocity);
 			playerY += yVelocity;
-		} else if (playerY == yLimit-20) {
-			yVelocity = 3;
+			playerX += xVelocity;
+		} 
+		//else if (playerY == yLimit-20) {
+		//	yVelocity = 3;
+		//}
+		else if(playerX > 70){
+			playerX -= 0.6;
+		}
+		if(playerY < 0){
+			playerY = 0.05;
+			yVelocity = 0.5;
 		}
 		
-		clearScreen();
-		drawRect(playerX, playerY, 10, 10);
+		//clearScreen();
+		//drawRect(playerX, playerY, 10, 10);
 		setTimeout(main, 10);
 	};
 
