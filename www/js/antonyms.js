dd7853fc594ade7fae1dc6dd0d81dff529acf3ab
JuAn David <david696-@hotmail.com>
2018-11-02 09:24:11 -0500

    Animación de selección optimizada

--- www/js/antonyms.js
+++ www/js/antonyms.js
@@ -13,12 +13,14 @@ var ma_card1, ma_card2, ma_card3, ma_card4, ma_card5, ma_card6, ma_card7, ma_car
 var ma_currentImg;
 var MA_COUNT;
 var MA_COUNTFINAL=4;
+var cards = [1,2,3,4,5,6,7,8];
+var cardsPairs = [[1,2],[2,1],[3,4],[4,3],[5,6],[6,5],[7,8],[8,7]];
 
 window.onload=init;
 
 function init(){
 
-    ma_card1=document.getElementById("ma_card1");
+    /*ma_card1=document.getElementById("ma_card1");
     ma_card2=document.getElementById("ma_card2");
     ma_card3=document.getElementById("ma_card3");
     ma_card4=document.getElementById("ma_card4");
@@ -56,10 +58,13 @@ function init(){
     ma_card5.addEventListener('click', verifyMatch);
     ma_card6.addEventListener('click', verifyMatch);
     ma_card7.addEventListener('click', verifyMatch);
-    ma_card8.addEventListener('click', verifyMatch);
+    ma_card8.addEventListener('click', verifyMatch);*/
 
     MA_COUNT=0;
 
+
+    renderCards();
+    setCardsDimensions();
 }
 
 function shuffle(a) {
@@ -73,18 +78,22 @@ function shuffle(a) {
     return a;
 }
 
-function verifyMatch(){
+function verifyMatch(e){
+    console.log(e);
+    var cardHeight = e.target.height;
+    var cardWidth = e.target.width;
     if(!ma_currentImg){
-        ma_currentImg=this;
-        new TweenMax.fromTo(ma_currentImg, 2, {width:"79%"},{width:"85%"});
-    }else if(ma_currentImg.id != this.id){
-        var ma_attr=ma_currentImg.getAttribute("data-ma");
-        var ma_sattr=this.getAttribute("data-ma");
-        if(!(ma_attr == ma_sattr)){
-            new TweenMax.fromTo(ma_currentImg, 2, {width:"85%"},{width:"70%"});
+        ma_currentImg=e.target;
+        new TweenMax.fromTo(ma_currentImg, 0.3, {width:cardWidth, height:cardHeight}, {width:cardHeight+10, height:cardHeight+10});
+    }else if(ma_currentImg.id != e.target.id){
+        /*var ma_attr=ma_currentImg.getAttribute("data-ma");
+        var ma_sattr=this.getAttribute("data-ma");*/
+        if(!cardsPairs.includes([ma_currentImg.id,e.target.id])){
+          console.log("no");
+            new TweenMax.fromTo(ma_currentImg, 0.3, {width:cardHeight+10, height:cardHeight+10}, {width:cardWidth, height:cardHeight});
         }else{
-            new TweenMax.fromTo(this, 2, {width:"79%"},{width:"85%"});
-            this.removeEventListener('click',verifyMatch);
+            new TweenMax.fromTo(e.target, 0.3, {width:cardWidth, height:cardHeight}, {width:cardHeight+10, height:cardHeight+10});
+            e.target.removeEventListener('click',verifyMatch);
             ma_currentImg.removeEventListener('click',verifyMatch);
             MA_COUNT++;
             if(MA_COUNT == MA_COUNTFINAL){
@@ -93,5 +102,36 @@ function verifyMatch(){
         }
         ma_currentImg=undefined;
     }
+
+}
+
+function renderCards() {
+  shuffle(cards);
+  var canvas = document.getElementById('cardsContainer');
+  var html = "";
+  var index;
+  for (var i = 0; i < 2; i++) {
+    html += '<div class="cardsRow">'
+    for (var j = 0; j < 4; j++) {
+      index = (i===0) ? j : j+4;
+      html += `<div id="cardContainer${cards[index]}" class="cardContainer"><img id="${cards[index]}" src="img/antonyms/${cards[index]}.png" class="card" onclick="verifyMatch(event);" /></div>`;
+    }
+    html += '</div>'
+  }
+  canvas.innerHTML = html;
+}
+
+function setCardsDimensions() {
+  var cardContainer = document.getElementById('cardContainer1');
+  var cardContainerStyles = window.getComputedStyle(cardContainer);
+  var cardHeight = cardContainer.clientHeight;
+  //var cardHeight = cardContainerStyles.getPropertyValue('height');
+  console.log(cardHeight);
+  var cards = document.querySelectorAll('.card');
+  for(var i=0; i<cards.length; i++){
+      cards[i].style.width = (cardHeight-15) + 'px';
+      cards[i].style.height = (cardHeight-15) + 'px';
+  }
+
 
 }
