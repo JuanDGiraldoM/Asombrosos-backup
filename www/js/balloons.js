d1d3ef0c79d1ffede0fea2e889d2cba5e8cf51be
SergioRodriguez317 <sergioarg17@gmail.com>
2018-11-05 01:18:51 -0500

    Random Balloons - New Implementation

--- www/js/balloons.js
+++ www/js/balloons.js
@@ -8,6 +8,7 @@ var hiddenBalloon;
 var partialScore = 0;
 var finalScore;
 var theBalloon;
+var colours = ["balMorado.png", "balRosa.png", "balVerde.png", "balAzul.png", "balLila.png", "balNaranja.png", "balRojo.png", "balSalmon.png","balMenta.png"];
 
 function init() {
     // introSection=document.getElementById('balloonIntro');
@@ -16,33 +17,52 @@ function init() {
     finalScore=document.getElementById('finalScore');
     gameSection = document.getElementById('balloonGame');
     scoreSection = document.getElementById('balloonsScoreSection');
+    this.balloons = 60;
+    this.balloonsArr = [];
    
-    var i = 0;
-    var cont = 100;
-    var ancho = screen.height;
-    for (i = 0; i < 6; i++) {
-        cont += 200;
-        document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon1' src='img/balloons/balMorado.png' style='right:" + (Math.floor(Math.random() * ancho / 100 * 25) + (Math.floor(Math.random() * ancho))) + "px '/>");
-        TweenMax.to("#balloon1", i + ((Math.floor(Math.random() * ancho)) * 0.01 - i), {ease: Power0.easeOut, y: -1000 });
-        document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon2' src='img/balloons/balRosa.png'style='right:" + (Math.floor(Math.random() * ancho / 100 * 35) + (Math.floor(Math.random() * ancho))) + "px '/>");
-        TweenMax.to("#balloon2", i + ((Math.floor(Math.random() * ancho)) * 0.01 - i), {ease: Power0.easeOut, y: -1000 });
-        document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon3' src='img/balloons/balVerde.png'style='right:" + (Math.floor(Math.random() * ancho / 100 * 45) + (Math.floor(Math.random() * ancho))) + "px '/>");
-        TweenMax.to("#balloon3", i + ((Math.floor(Math.random() * ancho)) * 0.01 - i), {ease: Power0.easeOut, y: -1000 });
-        document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon4' src='img/balloons/balAzul.png'style='right:" + (Math.floor(Math.random() * ancho / 100 * 55) + (Math.floor(Math.random() * ancho))) + "px '/>");
-        TweenMax.to("#balloon4", i + ((Math.floor(Math.random() * ancho)) * 0.01 - i), {ease: Power0.easeOut, y: -1000 });
-        document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon5' src='img/balloons/balLila.png'style='left:" + (Math.floor(Math.random() * ancho / 100 * 65) + (Math.floor(Math.random() * ancho))) + "px '/>");
-        TweenMax.to("#balloon5", i + ((Math.floor(Math.random() * ancho)) * 0.01 - i), {ease: Power0.easeOut, y: -1000 });
-        document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon6' src='img/balloons/balNaranja.png'style='left:" + (Math.floor(Math.random() * ancho / 100 * 75) + (Math.floor(Math.random() * ancho))) + "px '/>");
-        TweenMax.to("#balloon6", 1 + ((Math.floor(Math.random() * ancho)) * 0.01 - i), {ease: Power0.easeOut, y: -1000 });
-        document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon7' src='img/balloons/balRojo.png'style='left:" + (Math.floor(Math.random() * ancho / 100 * 85) + (Math.floor(Math.random() * ancho))) + "px '/>");
-        TweenMax.to("#balloon7", i + ((Math.floor(Math.random() * ancho)) * 0.01 - i), {ease: Power0.easeOut, y: -1000 });
-        document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon8' src='img/balloons/balSalmon.png'style='left:" + (Math.floor(Math.random() * ancho / 100 * 95) + (Math.floor(Math.random() * ancho))) + "px '/>");
-        TweenMax.to("#balloon8", i + ((Math.floor(Math.random() * ancho)) * 0.01 - i), {ease: Power0.easeOut, y: -1000 });
-        document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon9' src='img/balloons/balMenta.png'style='left:" + (Math.floor(Math.random() * ancho / 100 * 105) + (Math.floor(Math.random() * ancho))) + "px '/>");
-        TweenMax.to("#balloon9", i + ((Math.floor(Math.random() * ancho)) * 0.01 - i), {ease: Power0.easeOut, y: -1000 });
+    // var i = 0;
+    // var cont = 100;
+     var ancho = screen.height;
+    // for (i = 0; i < 2; i++) {
+    //     document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon1' src='img/balloons/balMorado.png' style='right:" + (Math.floor(Math.random() * ancho ) ) + "px '/>");
+    //     TweenMax.to("#balloon1", i + ((Math.floor(Math.random() * ancho)) * 0.01 +5), {ease: Power0.easeOut, y: -1000 });
+    //     document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon2' src='img/balloons/balRosa.png'style='right:" + (Math.floor(Math.random() * ancho ) ) + "px '/>");
+    //     TweenMax.to("#balloon2", i + ((Math.floor(Math.random() * ancho)) * 0.01 +5), {ease: Power0.easeOut, y: -1000 });
+    //     document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon3' src='img/balloons/balVerde.png'style='right:" + (Math.floor(Math.random() * ancho )) + "px '/>");
+    //     TweenMax.to("#balloon3", i + ((Math.floor(Math.random() * ancho)) * 0.01 +5), {ease: Power0.easeOut, y: -1000 });
+    //     document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon4' src='img/balloons/balAzul.png'style='right:" + (Math.floor(Math.random() * ancho) ) + "px '/>");
+    //     TweenMax.to("#balloon4", i + ((Math.floor(Math.random() * ancho)) * 0.01 +5), {ease: Power0.easeOut, y: -1000 });
+    //     document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon5' src='img/balloons/balLila.png'style='left:" + (Math.floor(Math.random() * ancho ) ) + "px '/>");
+    //     TweenMax.to("#balloon5", i + ((Math.floor(Math.random() * ancho)) * 0.01 +5), {ease: Power0.easeOut, y: -1000 });
+    //     document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon6' src='img/balloons/balNaranja.png'style='left:" + (Math.floor(Math.random() * ancho )) + "px '/>");
+    //     TweenMax.to("#balloon6", 1 + ((Math.floor(Math.random() * ancho)) * 0.01 +5), {ease: Power0.easeOut, y: -1000 });
+    //     document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon7' src='img/balloons/balRojo.png'style='left:" + (Math.floor(Math.random() * ancho)) + "px '/>");
+    //     TweenMax.to("#balloon7", i + ((Math.floor(Math.random() * ancho)) * 0.01 +5), {ease: Power0.easeOut, y: -1000 });
+    //     document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon8' src='img/balloons/balSalmon.png'style='left:" + (Math.floor(Math.random() * ancho ) ) + "px '/>");
+    //     TweenMax.to("#balloon8", i + ((Math.floor(Math.random() * ancho)) * 0.01 +5), {ease: Power0.easeOut, y: -1000 });
+    //     document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon9' src='img/balloons/balMenta.png'style='left:" + (Math.floor(Math.random() * ancho) ) + "px '/>");
+    //     TweenMax.to("#balloon9", i + ((Math.floor(Math.random() * ancho)) * 0.01 +5), {ease: Power0.easeOut, y: -1000 });
+    // }
+      
+    for (i = 0; i < this.balloons; i++) {
+        var newBalloon = new Balloon();
+        newBalloon.ball();
 
+    };
 
+    function Balloon(left, top) {
+        this.id = balloonsArr.length;
+      
+        this.animate = function() {
+            TweenMax.to("#balloon" + this.id, (Math.floor(Math.random() * ancho)) * 0.01 +3, {ease: Power0.easeOut, y: -1000});
+        }
+        this.ball = function() {
+            document.getElementById("theBalloon").insertAdjacentHTML('beforeend', "<img class='balloon' id='balloon"+this.id+"' src='img/balloons/"+colours[Math.floor(Math.random() * colours.length)]  +"' style='left:" + (Math.floor(Math.random() * 600 ) ) + "px '/>");  
+            balloonsArr.push(this);
+            this.animate();
+        }
     }
+
     theBalloon=document.getElementById('theBalloon');
     hiddenBalloon = theBalloon.getElementsByClassName('hidden');
     //TweenMax.to(".balloon", 10, { y: -1000});
