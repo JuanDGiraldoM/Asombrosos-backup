e7c61be30b1580a6b81a9eaac3daabeed2652c93
JuAn David <david696-@hotmail.com>
2018-11-08 23:01:23 -0500

    Lanzar win después de la última animación
    Se corrigió la llamada al método win para que se ejecute al terminar la última animación de la última carta seleccionada

--- www/js/antonyms.js
+++ www/js/antonyms.js
@@ -1,18 +1,8 @@
 
-/*var ma_arrayCard=[];
-ma_arrayCard[0]=["img/antonyms/anger.png","ma1"];
-ma_arrayCard[1]=["img/antonyms/calm.png","ma1"];
-ma_arrayCard[2]=["img/antonyms/fear.png","ma2"];
-ma_arrayCard[3]=["img/antonyms/pride.png","ma4"];
-ma_arrayCard[4]=["img/antonyms/happiness.png","ma3"];
-ma_arrayCard[5]=["img/antonyms/sadness.png","ma3"];
-ma_arrayCard[6]=["img/antonyms/surprise.png","ma2"];
-ma_arrayCard[7]=["img/antonyms/disappointment.png","ma4"];*/
-
-//var ma_card1, ma_card2, ma_card3, ma_card4, ma_card5, ma_card6, ma_card7, ma_card8;
 var ma_currentImg;
 var MA_COUNT;
 var MA_COUNTFINAL=4;
+var MA_PAIRS = 4;
 var cards = [1,2,3,4,5,6,7,8];
 var cardsPairs = [[1,2],[2,1],[3,4],[4,3],[5,6],[6,5],[7,8],[8,7]];
 var initialImg;
@@ -22,46 +12,6 @@ var cardHeight;
 var cardWidth;
 
 function openAntonymsGame(){
-
-    /*ma_card1=document.getElementById("ma_card1");
-    ma_card2=document.getElementById("ma_card2");
-    ma_card3=document.getElementById("ma_card3");
-    ma_card4=document.getElementById("ma_card4");
-    ma_card5=document.getElementById("ma_card5");
-    ma_card6=document.getElementById("ma_card6");
-    ma_card7=document.getElementById("ma_card7");
-    ma_card8=document.getElementById("ma_card8");
-
-    ma_arrayCard=shuffle(ma_arrayCard);
-
-    ma_card1.src=ma_arrayCard[0][0];
-    ma_card2.src=ma_arrayCard[1][0];
-    ma_card3.src=ma_arrayCard[2][0];
-    ma_card4.src=ma_arrayCard[3][0];
-    ma_card5.src=ma_arrayCard[4][0];
-    ma_card6.src=ma_arrayCard[5][0];
-    ma_card7.src=ma_arrayCard[6][0];
-    ma_card8.src=ma_arrayCard[7][0];
-
-    ma_card1.setAttribute('data-ma',ma_arrayCard[0][1]);
-    ma_card2.setAttribute('data-ma',ma_arrayCard[1][1]);
-    ma_card3.setAttribute('data-ma',ma_arrayCard[2][1]);
-    ma_card4.setAttribute('data-ma',ma_arrayCard[3][1]);
-    ma_card5.setAttribute('data-ma',ma_arrayCard[4][1]);
-    ma_card6.setAttribute('data-ma',ma_arrayCard[5][1]);
-    ma_card7.setAttribute('data-ma',ma_arrayCard[6][1]);
-    ma_card8.setAttribute('data-ma',ma_arrayCard[7][1]);
-
-
-
-    ma_card1.addEventListener('click', verifyMatch);
-    ma_card2.addEventListener('click', verifyMatch);
-    ma_card3.addEventListener('click', verifyMatch);
-    ma_card4.addEventListener('click', verifyMatch);
-    ma_card5.addEventListener('click', verifyMatch);
-    ma_card6.addEventListener('click', verifyMatch);
-    ma_card7.addEventListener('click', verifyMatch);
-    ma_card8.addEventListener('click', verifyMatch);*/
 
     var canvas = document.getElementById('cardsContainer');
 
@@ -103,21 +53,26 @@ function verifyMatch(e){
               borderColor:'rgba(15,94,94,0)', ease:Power0.easeOut});
         }else{
           lastImg = e.target;
+          MA_PAIRS--;
             new TweenMax.fromTo(e.target, 0.1, {width:cardWidth, height:cardHeight, borderColor:'rgba(15,94,94,0)', borderStyle:'none', ease:Power0.easeIn}, {width:cardHeight+10, height:cardHeight+10,
               borderColor:'rgba(15,94,94,1.0)', borderStyle:'solid', ease:Power0.easeOut, onComplete: function(){
-              changeCard = new TweenMax.to([initialImg, e.target], 0.7, {delay:1, rotationY:'-180', onComplete:resizeCards});
-              new TweenMax.set([initialImg, e.target], {delay:1.2, borderStyle:'none', onStart:changeForWood});
+              changeCard = new TweenMax.to([initialImg, e.target], 0.7, {delay:0.4, rotationY:'-180', onComplete:resizeCards});
+              new TweenMax.set([initialImg, e.target], {delay:0.6, borderStyle:'none', onStart:changeForWood});
               e.target.onclick = null;
               initialImg.onclick = null;
             }});
             MA_COUNT++;
-            if(MA_COUNT == MA_COUNTFINAL){
-                console.log("Ganaste");
-                Win();
-            }
+
         }
         ma_currentImg=null;
     }
+}
+
+function completePairs() {
+  if(MA_COUNT == MA_COUNTFINAL && MA_PAIRS == 0){
+      console.log("Ganaste");
+      Win();
+  }
 }
 
 function changeForWood() {
@@ -126,7 +81,7 @@ function changeForWood() {
 }
 
 function resizeCards() {
-  new TweenMax.fromTo([initialImg, lastImg], 0.05, {width:cardHeight+10, height:cardHeight+10, ease:Power0.easeIn}, {width:cardWidth, height:cardHeight, ease:Power0.easeOut});
+  new TweenMax.fromTo([initialImg, lastImg], 0.05, {width:cardHeight+10, height:cardHeight+10, ease:Power0.easeIn}, {width:cardWidth, height:cardHeight, ease:Power0.easeOut, onComplete:completePairs});
 }
 
 function renderCards() {
@@ -171,4 +126,3 @@ function maCountEnd(){
     lostGame();
   }
 }
-
